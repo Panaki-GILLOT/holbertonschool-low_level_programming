d4f4a066a48f960da59a57d0884c9e01e71a1bd1
panaki-cyber <gillotpanaki@gmail.com>
2025-11-19 19:10:45 +0000

    Fix: correct the script

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,13 @@
 #include <stddef.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name using a callback function
+ * @name: name to print
+ * @f: pointer to function that prints a name
+ *
+ * Return: Nothing.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
