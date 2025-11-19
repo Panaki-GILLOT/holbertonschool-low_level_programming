6b2f75a7f4895d35ae75e40ef789fb6bc8b49b73
panaki-cyber <gillotpanaki@gmail.com>
2025-11-19 19:16:56 +0000

    Feat: Add a function that searches for an integer.

--- function_pointers/main.c
+++ function_pointers/main.c
@@ -23,3 +23,19 @@ int main(void)
 
     return 0;
 }
+
+int is_98(int elem)
+{
+	return (elem == 98);
+}
+
+int main(void)
+{
+	int array[] = {10, 20, 98, 5, 2};
+	int index;
+
+	index = int_index(array, 5, is_98);
+	printf("Index: %d\n", index);
+
+	return (0);
+}
