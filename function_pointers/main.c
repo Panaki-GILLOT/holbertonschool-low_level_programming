af45805e11f5cb7e712aeb74bbf5fa6f91186451
panaki-cyber <gillotpanaki@gmail.com>
2025-11-19 19:19:08 +0000

    Fix: Correct the output

--- function_pointers/main.c
+++ function_pointers/main.c
@@ -1,29 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-void print_upper(char *s)
-{
-    while (*s)
-    {
-        putchar(toupper(*s));
-        s++;
-    }
-    putchar('\n');
-}
-
-void print_normal(char *s)
-{
-    printf("%s\n", s);
-}
-
-int main(void)
-{
-    print_name("Ghost", print_normal);
-    print_name("Ghost", print_upper);
-
-    return 0;
-}
-
 int is_98(int elem)
 {
 	return (elem == 98);
