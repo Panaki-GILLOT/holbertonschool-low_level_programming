bbd863a292e275834b026d1bab8c61b7434c075d
panaki-cyber <gillotpanaki@gmail.com>
2025-12-05 18:02:53 +0000

    Fix: the output of the program

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,28 +17,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!h)
 		return (NULL);
 
-	/* Insert at the beginning */
 	if (idx == 0)
-		return (add_dnodeint(h, n)); /* Assuming 2-add_dnodeint.c defines add_dnodeint */
+		return (add_dnodeint(h, n));
 
 	current = *h;
 	i = 0;
 
-	/* Traverse to the node just before the insertion point */
 	while (current != NULL && i < idx - 1)
 	{
 		current = current->next;
 		i++;
 	}
 
-	if (!current) /* Index is out of bounds */
+	if (!current)
 		return (NULL);
 
-	/* Insert at the end if current->next is NULL */
 	if (!current->next)
-		return (add_dnodeint_end(h, n)); /* Assuming 3-add_dnodeint_end.c defines add_dnodeint_end */
+		return (add_dnodeint_end(h, n));
 
-	/* Create new node */
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
