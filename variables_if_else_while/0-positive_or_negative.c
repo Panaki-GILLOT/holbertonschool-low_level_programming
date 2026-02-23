48a3f35b1045314242193514bfd44b82f798f436
panaki-gillot <gillotpanaki@gmail.com>
2026-02-23 14:43:31 +0000

    Fix: correct the output of the script 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 
 /**
- * main - Assign a random number to n and print if it is positive, negative
+ * main - assign a random number to n and prints if it is positive, negative,
  * or zero
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - (RAND_MAX / 2);
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 		printf("%d is positive\n", n);
