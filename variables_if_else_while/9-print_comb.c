b4627a0a58f6bf5a2f427cd4100d3bef44fe0f99
panaki-gillot <gillotpanaki@gmail.com>
2026-02-24 09:55:06 +0000

    Fix: Correct the output of the program to betty style

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
+/**
+ * main - prints all single digit combinations
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i <= 9; i++)
-    {
-        putchar(i + '0');
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
 
-        if (i != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
