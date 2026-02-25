f1a692b34078aae9d8bc54e5cd19fd36e463a52d
panaki-gillot <gillotpanaki@gmail.com>
2026-02-25 09:53:20 +0000

    Fix: correct the output of the description

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
-* main - function that prints the alphabet
+* print_alphabet - function that prints the alphabet
 *
 * Description: Uses _putchar to print all letters from 'a' to 'z',
 * followed by a new line.
 * Return: void
 */
-
 void print_alphabet(void)
 {
-char c;
+    char c;
 
-for (c = 'a'; c <= 'z'; c++)
-_putchar(c);
+    for (c = 'a'; c <= 'z'; c++)
+    _putchar(c);
 
-_putchar('\n');
+    _putchar('\n');
 }
