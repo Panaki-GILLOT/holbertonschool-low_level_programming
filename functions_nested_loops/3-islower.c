7cebd77adbda586f630d6eb4e0c7900e90a46efc
panaki-gillot <gillotpanaki@gmail.com>
2026-02-25 14:42:20 +0000

    fix: correct the output of the descript to betty doc style

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-* islower - function that checks for lowercase character.
-*
-* Description: function that checks for lowercase character.
-* Return 1 if c is lowercase
-* Return 0 otherwise
-*/
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
