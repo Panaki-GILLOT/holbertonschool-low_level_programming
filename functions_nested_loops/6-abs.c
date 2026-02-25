4b9cadfa148690dae31215756a9a41bc6fc6afbc
panaki-gillot <gillotpanaki@gmail.com>
2026-02-25 15:16:20 +0000

    Feat: Add a unction that computes the absolute value of an integer.

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the number to get absolute value of
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+return (n);
+}
