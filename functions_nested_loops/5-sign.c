6306d87fa18d76f02138ada92fc607f194f129d9
panaki-gillot <gillotpanaki@gmail.com>
2026-02-25 15:03:56 +0000

    Feat: Add a function that prints the sign of a number.

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 if n is greater than 0,
+ * 0 if n is equal to 0,
+ * -1 if n is less than 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+}
