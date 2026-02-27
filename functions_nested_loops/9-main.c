2fb8b67782a74ae7dd8bc1f7fc3c7d2a82ddeb28
panaki-gillot <gillotpanaki@gmail.com>
2026-02-27 09:40:00 +0000

    Fix: Correct the output of the last function

---
+++ functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - teste la fonction times_table
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    times_table();  /* Appel de ta fonction qui imprime la table de 9 */
+    return (0);
+}
---
+++ functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - écrit le caractère c sur la sortie standard
+ * @c: le caractère à écrire
+ *
+ * Return: 1 si succès, -1 sinon
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
