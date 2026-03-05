aa21b45b7f45940eb36d7725f52027c14d427166
Panaki-gillot <gillotmail.com>
2026-03-05 15:47:33 +0000

    Feat: Add a a function that convert a string to an integer.

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to the string
+ *
+ * Return: the integer value
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	unsigned int num = 0;
+	int found_digit = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+			;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			found_digit = 1;
+			num = num * 10 + (s[i] - '0');
+		}
+		else if (found_digit)
+			break;
+		i++;
+	}
+
+	return (num * sign);
+}
