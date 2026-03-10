3935ebf9c51eb73e431fa4d4971540419a5be7b9
panaki-gillot <gillotpanaki@gmail.com>
2026-03-10 09:09:58 +0000

    Initial commit

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a duplicated string
+ * @str: string to duplicate
+ *
+ * Return: pointer to the new string, or NULL if it fails
+ */
+
+char *_strdup(char *str)
+{
+	char *copy;
+	int len = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	copy = malloc(sizeof(char) * (len + 1));
+
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		copy[i] = str[i];
+	}
+
+	copy[len] = '\0';
+
+	return (copy);
+}
