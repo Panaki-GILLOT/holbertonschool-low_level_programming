0ba0fd5f3d9f994db30e4719f1bbddd833d28148
panaki-gillot <gillotpanaki@gmail.com>
2026-03-12 14:07:23 +0000

    Feat: Add all file about structure and typedef and add more prototype inside dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
 struct dog
 {
 	char *name;
@@ -9,5 +19,12 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
