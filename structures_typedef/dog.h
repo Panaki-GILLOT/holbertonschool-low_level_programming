59456813da650b618d73b20af8d3774559f1c683
panaki-gillot <gillotpanaki@gmail.com>
2026-03-12 14:00:30 +0000

    Fix: Add a function inside the dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,13 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
