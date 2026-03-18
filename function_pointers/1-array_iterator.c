9400afc1c2582bd78b29a96a8c079f114f9206d2
panaki-gillot <gillotpanaki@gmail.com>
2026-03-18 09:58:48 +0100

    Feat: implement int_index function to search for an integer in an array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,18 +2,18 @@
 #include <stddef.h>
 
 /**
- * array_iterator - executes a function on each element of an array
- * @array: pointer to the first element of the array
- * @size: number of elements in the array
- * @action: pointer to the function to execute on each element
- */
+* array_iterator - executes a function on each element of an array
+* @array: pointer to the first element of the array
+* @size: number of elements in the array
+* @action: pointer to the function to execute on each element
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+	size_t i;
 
-    if (array == NULL || action == NULL)
-        return;
+	if (array == NULL || action == NULL)
+		return;
 
-    for (i = 0; i < size; i++)
-        action(array[i]);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
